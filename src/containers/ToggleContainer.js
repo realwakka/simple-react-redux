19fc0c3fd514d4903564889ea470a595f8dd6a74
realwakka <realwakka@gmail.com>
2019-05-22 21:54:36 +0900

    refactor

--- src/containers/ToggleContainer.js
+++ src/containers/ToggleContainer.js
@@ -2,19 +2,21 @@ import { connect } from 'react-redux';
 import { toggle } from '../actions';
 import { Toggle } from '../components/Toggle';
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state, props) => {
     return {
         val: !state.val
     }
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch, props) => {
     return {
-        onClick: () => {
-            onToggle: (value) => { dispatch(toggle(value)) }
-        }
+        onToggle:
+            (value) => {
+                dispatch(toggle(value))
+
+            }
     }
-};
+}
 
 const ToggleContainer = connect(
     mapStateToProps,
